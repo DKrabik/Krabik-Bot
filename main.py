fe95d8c019d9f4cf6c5e2fed045dc8df245eb045
Denis Yatsko <brahms7549@gmail.com>
2020-05-28 04:29:20 +0300

    Fix bags

--- main.py
+++ main.py
@@ -45,28 +45,26 @@ async def help(ctx):
 # .sg_parse
 @tasks.loop(hours = 3)
 async def sg_parse(ctx):
-    while True:
-        channel = bot.get_channel(NEWS_ID)
-        last_title = await (channel.fetch_message(channel.last_message_id))
-        last_title = last_title.embeds[0].title
-        articles = parse(last_title)
-        if articles:
-            for article in articles[::-1]:
-                if article['text'] is None:
-                    print(f'''"[log] {article['title']}" не была опубликованна из-за больльшого количества символов''')
-                else:
-                    emb = discord.Embed(title=article['title'], colour=article['color'])
-                    emb.set_author(name=article['author'], icon_url=article['author_img'])
-                    emb.add_field(name='Дата:', value=article['date'])
-                    emb.add_field(name='Ссылка на источник:', value=article['link'])
-                    emb.add_field(name='Теги:', value=article['tags'])
-                    if article['img']:
-                        emb.set_image(url=article['img'])
-                    emb.description = article['text']
-                    await channel.send(embed=emb)
-                    print(f'''"[log] {article['title']}" - новость была успешно опубликованна''')
-        print("[log] Все новости опубликованны")
-        await asyncio.sleep(3600*3)
+    channel = bot.get_channel(NEWS_ID)
+    last_title = await (channel.fetch_message(channel.last_message_id))
+    last_title = last_title.embeds[0].title
+    articles = parse(last_title)
+    if articles:
+        for article in articles[::-1]:
+            if article['text'] is None:
+                print(f'''"[log] {article['title']}" не была опубликованна из-за больльшого количества символов''')
+            else:
+                emb = discord.Embed(title=article['title'], colour=article['color'])
+                emb.set_author(name=article['author'], icon_url=article['author_img'])
+                emb.add_field(name='Дата:', value=article['date'])
+                emb.add_field(name='Ссылка на источник:', value=article['link'])
+                emb.add_field(name='Теги:', value=article['tags'])
+                if article['img']:
+                    emb.set_image(url=article['img'])
+                emb.description = article['text']
+                await channel.send(embed=emb)
+                print(f'''"[log] {article['title']}" - новость была успешно опубликованна''')
+    print("[log] Все новости опубликованны")
 
 
 bot.run(TOKEN)
