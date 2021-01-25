75398c97193b6720373a398a4798d4b27c70919f
Krabik <60798304+DKrabik@users.noreply.github.com>
2021-01-25 14:18:54 +0200

    integrated PostgreSQL
    - integrated PostgreSQL
    - new functhion set_news_char_id

--- main.py
+++ main.py
@@ -3,16 +3,31 @@ from random import choice
 from os import environ
 from discord.ext import commands, tasks
 from sg_modules.parse import parse
+from psql import mypsql_commands
 
 TOKEN = environ.get('TOKEN')
 PREFIX = '.'
-NEWS_ID = int(environ.get('NEWS_ID'))
+HOST = environ.get('HOST')
+USER = environ.get('USER')
+PASSWORD = environ.get('PASSWORD')
+DATABASE = environ.get('DATABASE')
+
 
 bot = commands.Bot(command_prefix=PREFIX)
-
+connection = mypsql_commands.create_connection(HOST, USER, PASSWORD, DATABASE)
+connection.autocommit = True
 
 @bot.event
 async def on_ready():
+    if (connection):
+        mypsql_commands.query(connection, 'CREATE TABLE users(id BIGSERIAL NOT NULL PRIMARY KEY, user_id BIGINT NOT NULL, server_id BIGINT NOT NULL, nickname VARCHAR(32), money INT NOT NULL) ;')
+        mypsql_commands.query(connection, 'CREATE TABLE settings(server_id BIGINT NOT NULL PRIMARY KEY, news_chat_id BIGINT, last_article VARCHAR(100)) ;')
+        for guild in bot.guilds:
+            for member in guild.members:
+                if not member.bot:
+                    if (not mypsql_commands.query(connection, f'SELECT user_id, server_id FROM users WHERE user_id = {member.id} AND server_id = {member.guild.id} ;')):
+                        mypsql_commands.query(connection, f"INSERT INTO users(user_id, server_id, nickname, money) VALUES ({member.id}, {member.guild.id}, '{member}', 0)")             
+        mypsql_commands.query(connection, f"INSERT INTO settings(server_id) VALUES ({guild.id}) ON CONFLICT (server_id) DO NOTHING;")
     sg_parse.start()
     print("I'm ready!")
 
@@ -32,29 +47,44 @@ async def clear(ctx, amount=100):
     await ctx.channel.purge(limit=amount)
 
 
+# .set_news_chat_id
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def set_news_chat_id(ctx, msg = None):
+    try:
+        mypsql_commands.change_settings(connection, "news_chat_id", msg, ctx.guild.id)
+        if (mypsql_commands.select_from_settings(connection,"news_chat_id", ctx.guild.id) == int(msg)):
+            await ctx.message.add_reaction("✅")
+        else:
+            await ctx.message.add_reaction("❌")
+    except:
+        await ctx.message.add_reaction("❌")
+
+
 # .sg_parse
 @tasks.loop(hours=3)
-async def sg_parse():
-    channel = bot.get_channel(NEWS_ID)
-    last_title = await (channel.fetch_message(channel.last_message_id))
-    last_title = last_title.embeds[0].title
-    articles = parse(last_title)
-    if articles:
-        for article in articles[::-1]:
-            if article['text'] is None:
-                print(f'''"[log] {article['title']}" не была опубликованна из-за больльшого количества символов''')
-            else:
-                emb = discord.Embed(title=article['title'], colour=article['color'])
-                emb.set_author(name=article['author'], icon_url=article['author_img'])
-                emb.add_field(name='Дата:', value=article['date'])
-                emb.add_field(name='Ссылка на источник:', value=article['link'])
-                emb.add_field(name='Теги:', value=article['tags'])
-                if article['img']:
-                    emb.set_image(url=article['img'])
-                emb.description = article['text']
-                await channel.send(embed=emb)
-                print(f'''"[log] {article['title']}" - новость была успешно опубликованна''')
-    print("[log] Все новости опубликованны")
+async def sg_parse(ctx):
+    if (mypsql_commands.select_from_settings(connection, "news_chat_id", ctx.guild.id) == None):
+        channel = mypsql_commands.select_from_settings(connection, "news_chat_id", ctx.guild.id)
+        last_title = mypsql_commands.select_from_settings(connection, "last_article", ctx.guild.id)
+        articles = parse(last_title)
+        if articles:
+            for article in articles[::-1]:
+                if article['text'] is None:
+                    print(f'''"[log] {article['title']}" не была опубликованна из-за больльшого количества символов''')
+                else:
+                    emb = discord.Embed(title=article['title'], colour=article['color'])
+                    emb.set_author(name=article['author'], icon_url=article['author_img'])
+                    emb.add_field(name='Дата:', value=article['date'])
+                    emb.add_field(name='Ссылка на источник:', value=article['link'])
+                    emb.add_field(name='Теги:', value=article['tags'])
+                    if article['img']:
+                        emb.set_image(url=article['img'])
+                    emb.description = article['text']
+                    await channel.send(embed=emb)
+                    print(f'''"[log] {article['title']}" - новость была успешно опубликованна''')
+            mypsql_commands.change_settings(connection, "last_article", articles[0], ctx.guild.id)
+        print("[log] Все новости опубликованны")
 
 
 bot.run(TOKEN)
---
+++ psql/mypsql_commands.py
@@ -0,0 +1,41 @@
+import psycopg2
+from psycopg2 import Error
+
+def create_connection(host_name, user_name, user_password, db_name = None):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            host=host_name,
+            user=user_name,
+            password=user_password,
+            database=db_name
+        )
+        print(f"Connection to '{host_name}:{db_name}' successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+def query(connection, query):
+    cursor = connection.cursor()
+    temp = False
+    try:
+        cursor.execute(query)
+        if (query.find("INSERT") and query.find("UPDATE")):
+            temp = cursor.fetchall()
+        print(f"{query} - Query successful")
+    except Error as e:
+        print(f"{query} - The error '{e}' occurred")
+    finally:
+        return temp
+
+def select_from_settings(connection, select, server_id):
+    return query(connection, f'SELECT {select} FROM settings WHERE server_id = {server_id};')[0][0]
+
+def change_settings(connection, update, param, server_id):
+    return query(connection, f'UPDATE settings SET {update} = {param} WHERE server_id = {server_id}')
+
+    
+
+
+    
